ef3a2e29f0559b2acec8c72efe36aea6c1e6efe3
Ruan <cliff.kamau@student.moringaschool.com>
2023-08-21 15:39:19 +0300

    completed

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,8 @@
-def parrot():
-    pass
+def parrot(arg="Squawk!"):
+    print(arg)
+    return arg
+
+if __name__ == "__main__":
+    arg1 = "Hello, parrot!"
+    result1 = parrot(arg1)  
+    result2 = parrot()  
